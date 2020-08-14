3f832c7c988b07dd5581ab3596aa033a18198491
darthmorgot <dartmorgot@yandex.ru>
2020-08-14 14:57:20 +1000

    added methods getRoot and render to the class

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -3,4 +3,25 @@ export class Excel {
     this.$el = document.querySelector(selector);
     this.components = options.components || [];
   }
+
+  getRoot() {
+    const $root = document.createElement('div');
+    // $root.textContent = 'test';
+
+    this.components.forEach(Component => {
+      const component = new Component();
+      $root.insertAdjacentHTML('beforeend', component.toHTML());
+    });
+
+    return $root;
+  }
+
+  render() {
+    // this.$el.insertAdjacentHTML('afterbegin', `<h1>Test</h1>`);
+    // const node = document.createElement('h1');
+    // node.textContent = 'Test';
+    // this.$el.append(node);
+
+    this.$el.append(this.getRoot());
+  }
 }
