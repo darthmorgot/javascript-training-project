2875d7fdb74f747ba592999fc41bab782a2f8cd0
darthmorgot <dartmorgot@yandex.ru>
2020-08-14 16:32:04 +1000

    added static variable, changed toHTML method

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,7 +1,34 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 
 export class Table extends ExcelComponent {
+  static className = 'excel__table table';
+
   toHTML() {
-    return '<h1>Table</h1>';
+    return `
+      <div class="table__row row">
+        <div class="row__info"></div>
+        <div class="row__data columns">
+          <div class="columns__head">A</div>
+          <div class="columns__head">B</div>
+          <div class="columns__head">C</div>
+        </div>
+      </div>
+      <div class="table__row row">
+        <div class="row__info">1</div>
+        <div class="row__data cells">
+          <div class="cell selected" contenteditable>1</div>
+          <div class="cell" contenteditable>2</div>
+          <div class="cell" contenteditable>3</div>
+        </div>
+      </div>
+      <div class="table__row row">
+        <div class="row__info">2</div>
+        <div class="row__data cells">
+          <div class="cell" contenteditable>4</div>
+          <div class="cell" contenteditable>5</div>
+          <div class="cell" contenteditable>6</div>
+        </div>
+      </div>
+    `;
   }
 }
--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,7 +1,28 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 
 export class Toolbar extends ExcelComponent {
+  static className = 'excel__toolbar toolbar';
+
   toHTML() {
-    return '<h1>Toolbar</h1>';
+    return `
+      <button class="button">
+        <span class="material-icons">format_align_left</span>
+      </button>
+      <button class="button">
+        <span class="material-icons">format_align_center</span>
+      </button>
+      <button class="button">
+        <span class="material-icons">format_align_right</span>
+      </button>
+      <button class="button">
+        <span class="material-icons">format_bold</span>
+      </button>
+      <button class="button">
+        <span class="material-icons">format_italic</span>
+      </button>
+      <button class="button">
+        <span class="material-icons">format_underlined</span>
+      </button>
+    `;
   }
 }
