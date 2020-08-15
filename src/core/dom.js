84cd2ccbf47bebcce35757ff795a32ca5a873017
darthmorgot <dartmorgot@yandex.ru>
2020-08-15 16:26:14 +1000

    added constructor and methods to the dom class

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,8 +1,45 @@
 class Dom {
+  constructor(selector) {
+    this.$el = typeof selector === 'string'
+      ? document.querySelector(selector)
+      : selector;
+  }
+
+  html(html) {
+    if (typeof html === 'string') {
+      this.$el.innerHTML = html;
+      return this;
+    }
+
+    return this.$el.outerHTML.trim();
+  }
+
+  clear() {
+    this.html('');
+    return this;
+  }
+
+  append(node) {
+    if (node instanceof Dom) {
+      node = node.$el;
+    }
+
+    if (Element.prototype.append) {
+      this.$el.append(node);
+    } else {
+      this.$el.appendChild(node);
+    }
+
+    return this;
+  }
+
+  on() {
+
+  }
 }
 
-export function $() {
-  return new Dom();
+export function $(selector) {
+  return new Dom(selector);
 }
 
 $.create = (tagName, classes = '') => {
@@ -13,5 +50,5 @@ $.create = (tagName, classes = '') => {
     el.className = classes;
   }
 
-  return el;
+  return $(el);
 };
