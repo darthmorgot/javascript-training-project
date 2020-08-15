e4a2cf626725a2f90938834950b7ee6a793bc207
darthmorgot <dartmorgot@yandex.ru>
2020-08-15 17:05:46 +1000

    remove comments, changed code excel class

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -2,42 +2,30 @@ import {$} from '@core/dom';
 
 export class Excel {
   constructor(selector, options) {
-    // this.$el = document.querySelector(selector);
     this.$el = $(selector);
     this.components = options.components || [];
   }
 
   getRoot() {
-    // const $root = document.createElement('div');
-    // $root.classList.add('excel');
-
     const $root = $.create('div', 'excel');
 
-    this.components.forEach(Component => {
-      // const $el = document.createElement('div');
-      // // $el.classList.add(Component.className);
-      // $el.className = Component.className;
-
+    this.components = this.components.map(Component => {
       const $el = $.create('div', Component.className);
 
       const component = new Component($el);
 
-      // $el.innerHTML = component.toHTML();
       $el.html(component.toHTML());
 
-      // $root.insertAdjacentHTML('beforeend', component.toHTML());
       $root.append($el);
+
+      return component;
     });
 
     return $root;
   }
 
   render() {
-    // this.$el.insertAdjacentHTML('afterbegin', `<h1>Test</h1>`);
-    // const node = document.createElement('h1');
-    // node.textContent = 'Test';
-    // this.$el.append(node);
-
     this.$el.append(this.getRoot());
+    this.components.forEach(component => component.init());
   }
 }
