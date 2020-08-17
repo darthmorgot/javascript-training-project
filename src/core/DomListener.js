7c3cc1af84f33973efe70cb038fa0d6cdf3af821
darthmorgot <dartmorgot@yandex.ru>
2020-08-17 14:01:24 +1000

    added import and getMethodName function, updated initDOMListeners method

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,3 +1,5 @@
+import {capitalize} from '@core/utils';
+
 export class DomListener {
   constructor($root, listeners = []) {
     if (!$root) {
@@ -8,10 +10,29 @@ export class DomListener {
   }
 
   initDOMListeners() {
-    console.log(this.listeners);
+    // console.log(this.listeners, this.$root);
+
+    this.listeners.forEach(listener => {
+      // console.log(listener, this.$root);
+      const method = getMethodName(listener);
+      // console.log(method, this);
+
+      if (!this[method]) {
+        throw new Error(
+            `Method ${method} is not implemented in ${this.name} component`
+        );
+      }
+
+      // тоже самое, что и addEventListener
+      this.$root.on(listener, this[method].bind(this));
+    });
   }
 
   removeDOMListeners() {
 
   }
+}
+
+function getMethodName(evtName) {
+  return 'on' + capitalize(evtName);
 }
