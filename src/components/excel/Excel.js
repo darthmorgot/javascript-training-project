b975fa2f01a155562288d201d2f912337612e1d6
darthmorgot <dartmorgot@yandex.ru>
2020-08-17 14:43:05 +1000

    added debug

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -14,6 +14,11 @@ export class Excel {
 
       const component = new Component($el);
 
+      // debug
+      if (component.name) {
+        window['c' + component.name] = component;
+      }
+
       $el.html(component.toHTML());
 
       $root.append($el);
