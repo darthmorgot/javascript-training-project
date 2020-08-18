496a508fb913d90539687ec75d155385109ff9df
darthmorgot <dartmorgot@yandex.ru>
2020-08-18 12:04:59 +1000

    added methods - onClick, onMousedown, onMousemove, onMouseup

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -2,9 +2,32 @@ import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from '@/components/table/table.template';
 
 export class Table extends ExcelComponent {
+  constructor($root) {
+    super($root, {
+      name: 'Table',
+      listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
+    });
+  }
+
   static className = 'excel__table table';
 
   toHTML() {
     return createTable(20);
+  }
+
+  onClick() {
+    console.log('click');
+  }
+
+  onMousedown(evt) {
+    console.log('mousedown', evt.target.textContent);
+  }
+
+  onMousemove() {
+    console.log('mousemove');
+  }
+
+  onMouseup() {
+    console.log('mouseup');
   }
 }
