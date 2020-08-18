d02ef20be215f55fdd74865b616dfe4f5458e4ad
darthmorgot <dartmorgot@yandex.ru>
2020-08-18 15:41:22 +1000

    optimization performance

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -25,13 +25,17 @@ export class Table extends ExcelComponent {
       const coords = $parent.getCoords();
       // console.log($parent.data.column);
 
+      const cols = this.$root.findAll(`[data-column="${$parent.data.column}"]`);
+
+      let delta = 0;
+      let value = 0;
+
       document.onmousemove = e => {
-        const delta = e.pageX - coords.right;
-        const value = coords.width + delta;
+        delta = e.pageX - coords.right;
+        value = coords.width + delta;
         $parent.$el.style.width = value + 'px';
 
-        document.querySelectorAll(`[data-column="${$parent.data.column}"]`)
-          .forEach(el => el.style.width = value + 'px');
+        cols.forEach(el => el.style.width = value + 'px');
       };
 
       document.onmouseup = () => {
@@ -40,3 +44,10 @@ export class Table extends ExcelComponent {
     }
   }
 }
+
+// Scripting 317 ms  287 ms
+// Rendering 5081 ms 6444 ms
+// Painting  612 ms  895 ms
+// System    543 ms  924 ms
+// Idle      2786 ms 2268 ms
+// Total     9339 ms 10819 ms
