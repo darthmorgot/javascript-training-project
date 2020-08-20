4714f61b85d6b4e9f3d8bd3ca2e20c0eb5cf1ffa
darthmorgot <dartmorgot@yandex.ru>
2020-08-20 14:44:02 +1000

    updated toCell and createTable code

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,10 +3,28 @@ const CODES = {
   Z: 90
 };
 
-function toCell(_, index) {
-  return `
-    <div class="cell" data-column="${index}" contenteditable></div>
-  `;
+// function toCell(row, index) {
+//   return `
+//     <div
+//       class="cell"
+//       data-column="${index}"
+//       data-row="${row}"
+//       contenteditable>
+//       </div>
+//   `;
+// }
+
+function toCell(rowNum) {
+  return function(_, col) {
+    return `
+      <div
+        class="cell"
+        data-column="${col}"
+        data-id="${rowNum}:${col}"
+        contenteditable
+      ></div>
+    `;
+  };
 }
 
 function toColumn(heading, index) {
@@ -43,6 +61,7 @@ function toChar(_, index) {
 export function createTable(rowsCount = 10) {
   const colsCount = CODES.Z - CODES.A + 1;
   const rows = [];
+  let cells = '';
 
   const cols = new Array(colsCount)
     .fill('')
@@ -54,15 +73,16 @@ export function createTable(rowsCount = 10) {
     .map(toColumn)
     .join('');
 
-  const cells = new Array(colsCount)
-    .fill('')
-    .map(toCell)
-    .join('');
-
   rows.push(createRow('columns', cols));
 
-  for (let i = 1; i <= rowsCount; i++) {
-    rows.push(createRow(null, cells, i));
+  for (let row = 1; row <= rowsCount; row++) {
+    cells = new Array(colsCount)
+      .fill('')
+      // .map((_, col) => toCell(row, col))
+      .map(toCell(row))
+      .join('');
+
+    rows.push(createRow(null, cells, row));
   }
 
   return rows.join('');
