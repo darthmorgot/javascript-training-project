42a5b7a386acf49af14cac2938430972371d6283
darthmorgot <dartmorgot@yandex.ru>
2020-08-20 15:17:55 +1000

    added static className variable, clear method

--- src/components/table/TableSelection.js
+++ src/components/table/TableSelection.js
@@ -3,10 +3,18 @@ export class TableSelection {
     this.group = [];
   }
 
+  static className = 'selected';
+
+  clear() {
+    this.group.forEach($cell => $cell.removeClass(TableSelection.className));
+    this.group = [];
+  }
+
   // $el instance Dom === true
   select($el) {
+    this.clear();
     this.group.push($el);
-    $el.addClass('selected');
+    $el.addClass(TableSelection.className);
   }
 
   selectGroup() {
