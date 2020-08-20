b10a388041fed85e30d64f22f47a593a1a07369a
darthmorgot <dartmorgot@yandex.ru>
2020-08-20 17:18:35 +1000

    added range import and matrix function

--- src/components/table/table.functions.js
+++ src/components/table/table.functions.js
@@ -1,7 +1,23 @@
+import {range} from '@core/utils';
+
 export function shouldResize(evt) {
   return evt.target.dataset.resize;
 }
 
 export function isCell(evt) {
   return evt.target.dataset.type === 'cell';
+}
+
+export function matrix($target, $current) {
+  const target = $target.id(true);
+  const current = $current.id(true);
+
+  const cols = range(current.col, target.col);
+  const rows = range(current.row, target.row);
+  const ids = cols.reduce((acc, col) => {
+    rows.forEach(row => acc.push(`${row}:${col}`));
+    return acc;
+  }, []);
+
+  return ids;
 }
