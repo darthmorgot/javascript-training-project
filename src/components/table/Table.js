bb5e1ede44f5b0b4f6d99012a9fe22562e508b1a
darthmorgot <dartmorgot@yandex.ru>
2020-08-21 16:36:42 +1000

    added selectCell method, updated code

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -9,7 +9,7 @@ export class Table extends ExcelComponent {
   constructor($root, options) {
     super($root, {
       name: 'Table',
-      listeners: ['mousedown', 'keydown'],
+      listeners: ['mousedown', 'keydown', 'input'],
       ...options
     });
   }
@@ -28,10 +28,20 @@ export class Table extends ExcelComponent {
     super.init();
 
     const $cell = this.$root.find('[data-id="0:0"]');
-    this.selection.select($cell);
+    this.selectCell($cell);
+
     this.$on('formula:input', text => {
       this.selection.current.text(text);
     });
+
+    this.$on('formula:done', () => {
+      this.selection.current.focus();
+    });
+  }
+
+  selectCell($cell) {
+    this.selection.select($cell);
+    this.$emit('table:select', $cell);
   }
 
   onMousedown(evt) {
@@ -63,7 +73,11 @@ export class Table extends ExcelComponent {
 
       const id = this.selection.current.id(true);
       const $next = this.$root.find(nextSelector(key, id));
-      this.selection.select($next);
+      this.selectCell($next);
     }
+  }
+
+  onInput(evt) {
+    this.$emit('table:input', $(evt.target));
   }
 }
