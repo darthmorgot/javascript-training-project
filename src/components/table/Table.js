608f40523c775a066ba793aea9bf2c2443210055
darthmorgot <dartmorgot@yandex.ru>
2020-08-21 15:47:24 +1000

    updated init method

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -29,7 +29,7 @@ export class Table extends ExcelComponent {
 
     const $cell = this.$root.find('[data-id="0:0"]');
     this.selection.select($cell);
-    this.emitter.subscribe('is work', text => {
+    this.$on('formula:input', text => {
       this.selection.current.text(text);
     });
   }
