e4da4720e95f7c78c0c5c61b3f2929e5b1241fb6
darthmorgot <dartmorgot@yandex.ru>
2020-08-21 15:49:33 +1000

    added comments,  and  methods, updated destroy method

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,10 +5,12 @@ export class ExcelComponent extends DomListener {
     super($root, options.listeners);
     this.name = options.name || '';
     this.emitter = options.emitter;
+    this.unsubscribers = [];
 
     this.prepare();
   }
 
+  // настраиваем наш компонент до init
   prepare() {}
 
   // Возвращает шаблон компонента
@@ -16,11 +18,27 @@ export class ExcelComponent extends DomListener {
     return '';
   }
 
+  // инициализируем компонент
+  // добавляем DOM слушателей
   init() {
     this.initDOMListeners();
   }
 
+  // удаляем компонент
+  // чистим слушателей
   destroy() {
     this.removeDOMListeners();
+    this.unsubscribers.forEach(unsub => unsub());
+  }
+
+  // уведомляем слушателя о событии eventName
+  $emit(eventName, ...args) {
+    this.emitter.emit(eventName, ...args);
+  }
+
+  // подписываемся на событие eventName
+  $on(eventName, fn) {
+    const unsub = this.emitter.subscribe(eventName, fn);
+    this.unsubscribers.push(unsub);
   }
 }
