853acdb08a679b5d1ded0fee576ae5e74fc26f81
darthmorgot <dartmorgot@yandex.ru>
2020-08-23 16:11:23 +1000

    added store, storeSub variables, ,  methods

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,7 +5,9 @@ export class ExcelComponent extends DomListener {
     super($root, options.listeners);
     this.name = options.name || '';
     this.emitter = options.emitter;
+    this.store = options.store;
     this.unsubscribers = [];
+    this.storeSub = null;
 
     this.prepare();
   }
@@ -29,6 +31,7 @@ export class ExcelComponent extends DomListener {
   destroy() {
     this.removeDOMListeners();
     this.unsubscribers.forEach(unsub => unsub());
+    this.storeSub.unsubscribe();
   }
 
   // уведомляем слушателя о событии eventName
@@ -40,5 +43,13 @@ export class ExcelComponent extends DomListener {
   $on(eventName, fn) {
     const unsub = this.emitter.subscribe(eventName, fn);
     this.unsubscribers.push(unsub);
+  }
+
+  $dispatch(action) {
+    this.store.dispatch(action);
+  }
+
+  $subscribe(fn) {
+    this.storeSub = this.store.subscribe(fn);
   }
 }
