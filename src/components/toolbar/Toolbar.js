0c7931be61af4d8897967ad8c50375d314ec0759
darthmorgot <dartmorgot@yandex.ru>
2020-08-29 16:38:15 +1000

    added prepare, template and toHTML methods, updated onClick method

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,6 +1,8 @@
-import {ExcelComponent} from '@core/ExcelComponent';
+import {ExcelStateComponent} from '@core/ExcelStateComponent';
+import {createToolbar} from '@/components/toolbar/toolbar.template';
+import {$} from '@core/dom';
 
-export class Toolbar extends ExcelComponent {
+export class Toolbar extends ExcelStateComponent {
   constructor($root, options) {
     super($root, {
       name: 'Toolbar',
@@ -11,30 +13,34 @@ export class Toolbar extends ExcelComponent {
 
   static className = 'excel__toolbar toolbar';
 
+  prepare() {
+    const initialState = {
+      textAlign: 'left',
+      fontWeight: 'normal',
+      fontStyle: 'normal',
+      textDecoration: 'none'
+    };
+
+    this.initState(initialState);
+  }
+
+  get template() {
+    return createToolbar(this.state);
+  }
+
   toHTML() {
-    return `
-      <button class="button">
-        <span class="material-icons">format_align_left</span>
-      </button>
-      <button class="button">
-        <span class="material-icons">format_align_center</span>
-      </button>
-      <button class="button">
-        <span class="material-icons">format_align_right</span>
-      </button>
-      <button class="button">
-        <span class="material-icons">format_bold</span>
-      </button>
-      <button class="button">
-        <span class="material-icons">format_italic</span>
-      </button>
-      <button class="button">
-        <span class="material-icons">format_underlined</span>
-      </button>
-    `;
+    return this.template;
   }
 
   onClick(evt) {
-    console.log(evt.target);
+    const $trg = $(evt.target);
+
+    if ($trg.data.type === 'button') {
+      const value = JSON.parse($trg.data.value);
+      const key = Object.keys(value)[0];
+
+      this.setState({[key]: value[key]});
+      console.log(this.state);
+    }
   }
 }
