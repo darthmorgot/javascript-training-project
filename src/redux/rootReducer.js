744798c420ffb2fc6df3389222bfd731605a26d5
darthmorgot <dartmorgot@yandex.ru>
2020-08-30 16:42:12 +1000

    added value function, updated rootReducer function

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,20 +1,37 @@
-import {TABLE_RESIZE, CHANGE_TEXT} from './types';
+import {TABLE_RESIZE, CHANGE_TEXT, CHANGE_STYLE} from './types';
 
 export function rootReducer(state, action) {
-  let prevState;
+  // let prevState;
   let field;
 
   switch (action.type) {
     case TABLE_RESIZE:
       field = action.data.type === 'column' ? 'colState' : 'rowState';
-      prevState = state[field] || {};
-      prevState[action.data.id] = action.data.value;
-      return {...state, [field]: prevState};
+      // prevState = state[field] || {};
+      // prevState[action.data.id] = action.data.value;
+      return {
+        ...state,
+        [field]: value(state, field, action)
+      };
     case CHANGE_TEXT:
-      prevState = state['dataState'] || {};
-      prevState[action.data.id] = action.data.value;
-      return {...state, currentText: action.data.value, dataState: prevState};
+      field = 'dataState';
+      // prevState = state[field] || {};
+      // prevState[action.data.id] = action.data.value;
+      return {
+        ...state,
+        currentText: action.data.value,
+        [field]: value(state, field, action)
+      };
+    case CHANGE_STYLE:
+      return {...state, currentStyles: action.data};
     default:
       return state;
   }
+}
+
+function value(state, field, action) {
+  const val = state[field] || {};
+  val[action.data.id] = action.data.value;
+
+  return val;
 }
