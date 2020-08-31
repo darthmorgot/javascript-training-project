3866a0087166674bcaa179f1d7ef783867d7326d
darthmorgot <dartmorgot@yandex.ru>
2020-08-31 22:47:37 +1000

    updated code

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,9 @@
-import {TABLE_RESIZE, CHANGE_TEXT, CHANGE_STYLE} from './types';
+import {TABLE_RESIZE, CHANGE_TEXT, CHANGE_STYLE, APPLY_STYLE} from './types';
 
 export function rootReducer(state, action) {
   // let prevState;
   let field;
+  let val;
 
   switch (action.type) {
     case TABLE_RESIZE:
@@ -24,6 +25,17 @@ export function rootReducer(state, action) {
       };
     case CHANGE_STYLE:
       return {...state, currentStyles: action.data};
+    case APPLY_STYLE:
+      field = 'stylesState';
+      val = state[field] || {};
+      action.data.ids.forEach(id => {
+        val[id] = {...val[id], ...action.data.value};
+      });
+      return {
+        ...state,
+        [field]: val,
+        currentStyles: {...state.currentStyles, ...action.data.value}
+      };
     default:
       return state;
   }
