3014416aa56c0f20785a9ac3d41e51ba5792374b
darthmorgot <dartmorgot@yandex.ru>
2020-09-01 10:51:14 +1000

    added changeTitle method

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,10 @@
-import {TABLE_RESIZE, CHANGE_TEXT, CHANGE_STYLE, APPLY_STYLE} from './types';
+import {
+  TABLE_RESIZE,
+  CHANGE_TEXT,
+  CHANGE_STYLE,
+  APPLY_STYLE,
+  CHANGE_TITLE
+} from './types';
 
 // action creator
 export function tableResize(data) {
@@ -25,6 +31,13 @@ export function changeStyles(data) {
 export function applyStyle(data) {
   return {
     type: APPLY_STYLE,
+    data
+  };
+}
+
+export function changeTitle(data) {
+  return {
+    type: CHANGE_TITLE,
     data
   };
 }
