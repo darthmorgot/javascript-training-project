25da31a962272cdbf5f25f623a1ebd7bd49aa471
darthmorgot <dartmorgot@yandex.ru>
2020-09-01 11:16:09 +1000

    minor edits in code

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -60,7 +60,7 @@ export class Table extends ExcelComponent {
     this.$emit('table:select', $cell);
 
     const styles = $cell.getStyles(Object.keys(defaultStyles));
-    console.log('Styles to dispatch', styles);
+    // console.log('Styles to dispatch', styles);
     this.$dispatch(actions.changeStyles(styles));
   }
 
