ad80b1c46884e25db79fd3659cfb395ae79fba64
darthmorgot <dartmorgot@yandex.ru>
2020-09-02 13:26:46 +1000

    updated changePageHandler method

--- src/core/routes/Router.js
+++ src/core/routes/Router.js
@@ -1,5 +1,5 @@
 import {$} from '@core/dom';
-// import {ActiveRoute} from './ActiveRoute';
+import {ActiveRoute} from './ActiveRoute';
 
 export class Router {
   constructor(selector, routes) {
@@ -9,6 +9,7 @@ export class Router {
 
     this.$placeholder = $(selector);
     this.routes = routes;
+    this.page = null;
 
     this.changePageHandler = this.changePageHandler.bind(this);
     this.init();
@@ -20,12 +21,21 @@ export class Router {
   }
 
   changePageHandler() {
-    // const Page = this.routes.dashboard;
-    const Page = this.routes.excel;
-    const page = new Page();
-    this.$placeholder.append(page.getRoot());
+    if (this.page) {
+      this.page.destroy();
+    }
 
-    page.afterRender();
+    this.$placeholder.clear();
+
+    const Page = ActiveRoute.path.includes('excel')
+     ? this.routes.excel
+     : this.routes.dashboard;
+
+    this.page = new Page(ActiveRoute.param);
+
+    this.$placeholder.append(this.page.getRoot());
+
+    this.page.afterRender();
   }
 
   destroy() {
