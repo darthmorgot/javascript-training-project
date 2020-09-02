133994a1a1d885ccc0f934df159878a8fa5439ae
darthmorgot <dartmorgot@yandex.ru>
2020-09-02 13:27:52 +1000

    updated code

--- src/pages/ExcelPage.js
+++ src/pages/ExcelPage.js
@@ -4,14 +4,15 @@ import {Header} from '@/components/header/Header';
 import {Toolbar} from '@/components/toolbar/Toolbar';
 import {Formula} from '@/components/formula/Formula';
 import {Table} from '@/components/table/Table';
-import {CreateStore} from '@core/createStore';
+import {createStore} from '@core/createStore';
 import {rootReducer} from '@/redux/rootReducer';
 import {storage, debounce} from '@core/utils';
 import {initialState} from '@/redux/initialState';
 
 export class ExcelPage extends Page {
   getRoot() {
-    const store = new CreateStore(rootReducer, initialState);
+    console.log(this.params);
+    const store = createStore(rootReducer, initialState);
 
     const stateListener = debounce(state => {
       storage('excel-state', state);
