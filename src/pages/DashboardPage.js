c7ee795a5cd6d200ecfc31295bc96bbf5d6e5c79
darthmorgot <dartmorgot@yandex.ru>
2020-09-02 14:35:09 +1000

    updated getRoot method

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,8 +1,11 @@
 import {Page} from '@core/Page';
 import {$} from '@core/dom';
+import {createRecordsTable} from './dashboard.functions';
 
 export class DashboardPage extends Page {
   getRoot() {
+    const now = Date.now().toString();
+
     return $.create('div', 'db').html(`
       <div class="db__header">
         <h1>Excel dashboard</h1>
@@ -10,30 +13,12 @@ export class DashboardPage extends Page {
 
       <div class="db__new">
         <div class="db__view">
-          <a href="#" class="db__create">New table</a>
+          <a href="#excel/${now}" class="db__create">New table</a>
         </div>
       </div>
 
       <div class="db__table db__view list">
-        <div class="list__header">
-          <span class="list__title">Name table</span>
-          <span class="list__date">Opening date</span>
-        </div>
-
-        <ul class="list__table">
-          <li class="list__record">
-            <a href="#">Table 1</a>
-            <span>13.08.2020</span>
-          </li>
-          <li class="list__record">
-            <a href="#">Table 2</a>
-            <span>13.08.2020</span>
-          </li>
-          <li class="list__record">
-            <a href="#">Table 3</a>
-            <span>13.08.2020</span>
-          </li>
-        </ul>
+        ${createRecordsTable()}
       </div>
     `);
   }
